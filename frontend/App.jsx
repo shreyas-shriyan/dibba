93ed46d68887de640dc75fcab5cf89c2330fcd92
shreyas <shreyshriyan@gmail.com>
2025-10-07 00:18:09 +0530

    integrated container start, stop, and status update functions

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -1,15 +1,12 @@
 import { signal, effect } from "@preact/signals";
 import { ContainerService } from "./bindings/github.com/shreyas-shriyan/dibba";
+import { Events } from "@wailsio/runtime";
 
 const isContainerRunning = signal(false);
 
-const isContainerServiceRunning = async () => {
-  return await ContainerService.CheckContainerServiceRunning();
-};
-
-// const startContainerService = () => {};
-
-// const stopContainerService = () => {};
+Events.On("containerStatus", (value) => {
+  isContainerRunning.value = value.data[0];
+});
 
 export function App() {
   // check container service status
@@ -23,29 +20,29 @@ export function App() {
   return (
     <div class="container text-center mx-auto max-w-4xl p-4 pt-2">
       <h1 class="text-2xl font-bold text-success">Dibba</h1>
-      <div
-        class="alert alert-info mb-4 flex justify-between"
-        id="containerStatus"
-      >
+      <div class="alert alert-info mb-4 mt-2 flex justify-between">
         <span>
           Container service status:{" "}
           <span id="containerStatusText">
-            {isContainerRunning ? "Running" : "Not Running"}
+            {isContainerRunning.value ? "Running" : "Not Running"}
           </span>
         </span>
         <div class="flex gap-2">
-          <button
-            className="btn btn-success"
-            onClick={() => ContainerService.StartContainerService()}
-          >
-            Start
-          </button>
-          <button
-            className="btn btn-error"
-            onClick={() => ContainerService.StopContainerService()}
-          >
-            Stop
-          </button>
+          {isContainerRunning.value ? (
+            <button
+              className="btn btn-error"
+              onClick={() => ContainerService.StopContainerService()}
+            >
+              Stop
+            </button>
+          ) : (
+            <button
+              className="btn btn-success"
+              onClick={() => ContainerService.StartContainerService()}
+            >
+              Start
+            </button>
+          )}
         </div>
       </div>
     </div>
